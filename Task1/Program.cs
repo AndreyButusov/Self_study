4a5f26415a3f9dd6521dad6fb4e36866dd0a1ea8
Andrey Butusov <agbutusov@mail.ru>
2024-01-10 23:26:08 +0300

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задание с клавиатуры число элементов массива
+
+int n;
+Console.WriteLine("Введите число элементов массива");
+n = Convert.ToInt16(Console.ReadLine());
+// Объявим одномерный массив Data с n-элементами
+double[] data = new double[n];
+int i = 0;
+while (i < n)
+{
+    Console.WriteLine("Введите элемент массива");
+    data[i] = double.Parse(Console.ReadLine());
+    i++;
+}
+// Вывод содержимого массива
+for (i=0; i<n; i++)
+Console.WriteLine("Элемент ["+i+"]:" + data[i]);
+Console.ReadKey();
